dc16f3c3c3132217204aee461a8816ec0d93cfb8
nbhaohao <nbhaohao@163.com>
2022-05-22 17:05:10 +0800

    Unicode practice

---
+++ chapter-6/src/Unicode.java
@@ -0,0 +1,9 @@
+public class Unicode {
+    public static void main(String[] args) {
+        // https://unicode-table.com/cn/2764/
+        // 十六进制：2764，十进制是：10084
+        char letter = 10084;
+        // ❤
+        System.out.println(letter);
+    }
+}
