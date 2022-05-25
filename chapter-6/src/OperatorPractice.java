a7257600d74fd2a4e2b44c6f94c30361a1635785
nbhaohao <nbhaohao@163.com>
2022-05-25 21:41:20 +0800

    Operator practice

--- chapter-6/src/OperatorPractice.java
+++ chapter-6/src/OperatorPractice.java
@@ -7,7 +7,24 @@ public class OperatorPractice {
         System.out.println("thousand: " + number / 1000);
     }
 
+    public static void trickyOne() {
+        int a = 5;
+        System.out.println(a++ + a++); // 11
+        // a = 7
+        System.out.println(a++ + ++a); // 16
+        // a = 9
+        System.out.println(++a + ++a); // 21
+    }
+
     public static void main(String[] args) {
         printNumberPosition(1234);
+        trickyOne();
+        int a = 5;
+        int d = 7;
+        int b = a++;
+        System.out.println("b:" + b);
+        int c = ++d;
+        System.out.println("c:" + c);
+
     }
 }
