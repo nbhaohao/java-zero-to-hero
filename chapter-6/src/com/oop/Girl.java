8c4572cddd3dfab9591059634a78fb10b69ed124
nbhaohao <nbhaohao@163.com>
2022-06-01 21:25:32 +0800

    Get/Set Practice

---
+++ chapter-6/src/com/oop/Girl.java
@@ -0,0 +1,13 @@
+package com.oop;
+
+public class Girl {
+    private int age;
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ chapter-6/src/com/oop/Test.java
@@ -0,0 +1,9 @@
+package com.oop;
+
+public class Test {
+    public static void main(String[] args) {
+        Girl girl = new Girl();
+        girl.setAge(31);
+        System.out.println(girl.getAge());
+    }
+}
