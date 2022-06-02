8ed45072c6561798950e484dde57d8fc50ca718e
nbhaohao <nbhaohao@163.com>
2022-06-02 21:39:49 +0800

    ToString Practice

---
+++ chapter-6/src/com/oop6/Test.java
@@ -0,0 +1,9 @@
+package com.oop6;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student("ff", 19, 160.7);
+        System.out.println(student.toString());
+        System.out.println(student);
+    }
+}
