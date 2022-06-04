f3f87d4170222a7c34cc94c0b0929a507e4760f7
nbhaohao <nbhaohao@163.com>
2022-06-04 21:31:17 +0800

    StringBuilder Practice

---
+++ TestCommonClass/src/com/pudge/test01/StringTest.java
@@ -0,0 +1,14 @@
+package com.pudge.test01;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String name = "zzh";
+        System.out.println(name);
+        String string = new String();
+        String string2 = new String("123");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("dsadasdasdas");
+        System.out.println(stringBuilder.toString());
+
+    }
+}
