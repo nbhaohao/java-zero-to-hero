b177676f155f05cc53244023452b91de2b0bc55f
nbhaohao <nbhaohao@163.com>
2022-06-11 17:04:42 +0800

    Serializable

---
+++ TestFile/src/com/pudge/file/Test12.java
@@ -0,0 +1,47 @@
+package com.pudge.file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+class Person implements Serializable {
+    private String name;
+
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class Test12 {
+    public static void main(String[] args) {
+        File file = new File("fields");
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file));
+             ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))) {
+            objectOutputStream.writeObject(new Person("xx"));
+            Person person = (Person) objectInputStream.readObject();
+            System.out.println(person);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
