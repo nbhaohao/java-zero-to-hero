4f58a0acadd1a5876fd3850ac06728d7f936017d
nbhaohao <nbhaohao@163.com>
2022-06-12 18:39:57 +0800

    TestTCPServer

--- TestNetwork/src/com/pudge/tcp/TestClient.java
+++ TestNetwork/src/com/pudge/tcp/TestClient.java
@@ -1,5 +1,6 @@
 package com.pudge.tcp;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
@@ -7,8 +8,10 @@ import java.net.Socket;
 public class TestClient {
     public static void main(String[] args) {
         try (Socket socket = new Socket("192.168.3.32", 8888);
-             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream())) {
+             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream())) {
             dataOutputStream.writeUTF("你好");
+            System.out.println(dataInputStream.readUTF());
         } catch (IOException e) {
             e.printStackTrace();
         }
--- TestNetwork/src/com/pudge/tcp/TestServer.java
+++ TestNetwork/src/com/pudge/tcp/TestServer.java
@@ -1,15 +1,19 @@
 package com.pudge.tcp;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TestServer {
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(8888); Socket socket = serverSocket.accept();
-             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream())) {
+        try (ServerSocket serverSocket = new ServerSocket(8888);
+             Socket socket = serverSocket.accept();
+             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream())) {
             System.out.println(dataInputStream.readUTF());
+            dataOutputStream.writeUTF("response");
         } catch (IOException e) {
             e.printStackTrace();
         }
