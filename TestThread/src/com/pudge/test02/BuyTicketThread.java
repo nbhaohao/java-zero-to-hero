78f2b2a3c898746aa833c8dc4a4bd964dc1fe954
nbhaohao <nbhaohao@163.com>
2022-06-12 10:06:31 +0800

    Lock

--- TestThread/src/com/pudge/test02/BuyTicketThread.java
+++ TestThread/src/com/pudge/test02/BuyTicketThread.java
@@ -1,16 +1,26 @@
 package com.pudge.test02;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BuyTicketThread implements Runnable {
-    int ticketNum = 10;
+    int ticketNum = 100;
+    Lock lock = new ReentrantLock();
 
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
-            synchronized (this) {
+            lock.lock();
+            try {
                 if (ticketNum > 0) {
                     System.out.println(Thread.currentThread().getName() + "我买到了从北京到哈尔滨的第" + ticketNum + "张车票");
                     ticketNum--;
                 }
+
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            } finally {
+                lock.unlock();
             }
 
         }
