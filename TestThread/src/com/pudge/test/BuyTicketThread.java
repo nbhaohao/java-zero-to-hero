e2efef01c2fe39fb96412a1a57254b81230ea635
nbhaohao <nbhaohao@163.com>
2022-06-12 09:40:54 +0800

    synchronized block

--- TestThread/src/com/pudge/test/BuyTicketThread.java
+++ TestThread/src/com/pudge/test/BuyTicketThread.java
@@ -1,7 +1,7 @@
 package com.pudge.test;
 
 public class BuyTicketThread extends Thread {
-    static int ticketNum = 10;
+    static int ticketNum = 1000;
 
     public BuyTicketThread(String name) {
         super(name);
@@ -9,10 +9,12 @@ public class BuyTicketThread extends Thread {
 
     @Override
     public void run() {
-        for (int i = 0; i < 100; i++) {
-            if (ticketNum > 0) {
-                System.out.println(this.getName() + "我买到了从北京到哈尔滨的第" + ticketNum + "张车票");
-                ticketNum--;
+        for (int i = 0; i < 1000; i++) {
+            synchronized (BuyTicketThread.class) {
+                if (ticketNum > 0) {
+                    System.out.println(this.getName() + "我买到了从北京到哈尔滨的第" + ticketNum + "张车票");
+                    ticketNum--;
+                }
             }
         }
     }
--- TestThread/src/com/pudge/test02/BuyTicketThread.java
+++ TestThread/src/com/pudge/test02/BuyTicketThread.java
@@ -6,10 +6,13 @@ public class BuyTicketThread implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
-            if (ticketNum > 0) {
-                System.out.println(Thread.currentThread().getName() + "我买到了从北京到哈尔滨的第" + ticketNum + "张车票");
-                ticketNum--;
+            synchronized (this) {
+                if (ticketNum > 0) {
+                    System.out.println(Thread.currentThread().getName() + "我买到了从北京到哈尔滨的第" + ticketNum + "张车票");
+                    ticketNum--;
+                }
             }
+
         }
     }
 }
