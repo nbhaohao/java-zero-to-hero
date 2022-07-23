eb4e821ad94e57705135da24d39834098c282a83
nbhaohao <nbhaohao@163.com>
2022-07-23 11:25:38 +0800

    JDBC demo

--- TestJDBC/src/com/pudge/test1/TestJDBC.java
+++ TestJDBC/src/com/pudge/test1/TestJDBC.java
@@ -4,19 +4,34 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.function.Consumer;
 
 public class TestJDBC {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException {
         // Driver will register by itself
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection(
+        testDelete();
+    }
+
+    public static void testSQL(Consumer<Statement> consumer) {
+        try (Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/mytestdb?useSSL=false&useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai",
-                "root", "123456");
-        Statement statement = connection.createStatement();
-        String createNewDepartmentSQL = "INSERT INTO dept VALUES(50, '教学部', '北京');";
-        int rows = statement.executeUpdate(createNewDepartmentSQL);
-        System.out.println("effect rows:" + rows);
-        statement.close();
-        connection.close();
+                "root", "123456"); Statement statement = connection.createStatement()) {
+            consumer.accept(statement);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void testDelete() {
+        testSQL(statement -> {
+            String sql = "DELETE FROM dept WHERE deptno = 555;";
+            try {
+                int rows = statement.executeUpdate(sql);
+                System.out.println(rows);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        });
     }
 }
